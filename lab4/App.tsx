e74968f1bcb1a51f8bc595987e133736712778f1
Sagar Patel <sagar.patel@willowtreeapps.com>
2022-06-07 15:31:26 -0400

    display image of food with btn

--- lab4/App.tsx
+++ lab4/App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Pressable, View, Image, Text } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [image, setImage] = useState('');
+  const [hasimage, setHasImage] = useState(false);
+
+  async function fetchImage(){
+    let response = await fetch('https://foodish-api.herokuapp.com/api/');
+    response = await response.json();
+    setImage(response.image);
+    setHasImage(!hasimage)
+  };
+
+  useEffect(() => {
+    !hasimage && fetchImage();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        hasimage ?
+        <View>
+            <Image
+              style={styles.img}
+              source={{uri: image}}
+            />
+            <Pressable onPress={fetchImage} style={styles.btn}>
+              <Text style={{color: 'white',}}>New Image</Text>
+            </Pressable>
+          </View>
+          :
+          <Pressable onPress={fetchImage} style={styles.btn}>
+            <Text style={{color: 'white',}}>New Image</Text>
+          </Pressable>
+      }
     </View>
   );
 }
@@ -17,4 +46,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: 150,
+    height: 150,
+  },
+  btn: {
+      marginTop: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 3,
+      backgroundColor: 'black',
+  }
 });
